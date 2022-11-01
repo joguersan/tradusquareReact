ea9e4be71367843f7e0bb387c00497df693261b6
joguersan <joguersan@gmail.com>
2022-11-01 21:06:02 +0100

    :pencil: Added propTypes

--- src/components/navbar/NavbarButton.jsx
+++ src/components/navbar/NavbarButton.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
 
-const NavbarButton = ({ icon, text, href, }) => {
-  return (
-    <li className='nav-item'>
-      <a className='nav-link' href={href}>
-        <i className={`bi bi-${ icon } me-2`}></i> {text}
-      </a>
-    </li>
-  )
-}
+const NavbarButton = ({ icon, text, href }) => (
+  <li className='nav-item'>
+    <a className='nav-link' href={href}>
+      <i className={`bi bi-${ icon } me-2`}></i> {text}
+    </a>
+  </li>
+)
 
 NavbarButton.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  href: PropTypes.string
 }
 
 NavbarButton.defaultProps = {
